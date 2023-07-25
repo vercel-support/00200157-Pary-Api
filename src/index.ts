43a24aee1f1b52b72956b5baf269661c6b8542b1
BarraR3port <barrar3port@gmail.com>
2023-07-25 00:44:08 -0400

    feat: Added tokens to the auth system

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import { PrismaClient } from '@prisma/client';
 import express from 'express';
 import { GoogleUser } from '../types';
+import jwt from 'jsonwebtoken';
 
-const { STREAM_API_KEY = '', STREAM_API_SECRET } = process.env;
+const { JWT_SECRET, JWT_REFRESH_SECRET } = process.env;
+
+if (JWT_SECRET === undefined) {
+    throw new Error('No JWT_SECRET env variable found.');
+}
+
+if (JWT_REFRESH_SECRET === undefined) {
+    throw new Error('No JWT_REFRESH_SECRET env variable found.');
+}
 
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
+
+function extractToken(bearerToken: string): string {
+    return bearerToken.split(' ')[1];
+}
 
 app.post('/login', async (req, res) => {
     if (req.body.googleUser === undefined) {
@@ -17,31 +30,142 @@ app.post('/login', async (req, res) => {
     const googleUser = req.body.googleUser as GoogleUser;
 
     const user = await prisma.user.upsert({
-        where: { username: googleUser.name.replace(' ', '') },
+        where: {
+            assignedGoogleID: googleUser.id,
+        },
         create: {
-            username: googleUser.name.replace(' ', ''),
+            username: "",
             name: googleUser.name,
             email: googleUser.email,
             lastName: googleUser.family_name,
             assignedGoogleID: googleUser.id,
+            profilePicture: googleUser.picture,
         },
         update: {
-            username: googleUser.name.replace(' ', ''),
-            name: googleUser.name,
-            email: googleUser.email,
-            lastName: googleUser.family_name,
         },
     });
 
+    const accessToken = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '15m' });
+    const refreshToken = jwt.sign({ id: user.id }, JWT_REFRESH_SECRET, { expiresIn: '7d' });
+
+    await prisma.user.update({
+        where: { id: user.id },
+        data: {
+            accessToken,
+            refreshToken
+        }
+    });
+
     return res.status(200).json({
-        user
+        user,
+        accessToken,
+        refreshToken,
     });
 });
 
 app.get('/users', async (req, res) => {
-    const users = await prisma.user.findMany();
-    return res.json(users);
+    const bearerToken = req.headers['authorization'];
+
+    if (!bearerToken) {
+        return res.status(403).json({ error: 'No token provided.' });
+    }
+
+    const token = extractToken(bearerToken);
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        const users = await prisma.user.findMany(
+            {
+                select: {
+                    id: true,
+                    name: true,
+                    lastName: true,
+                    email: true,
+                    profilePicture: true,
+                },
+            }
+        );
+        return res.json(users);
+    } catch (error) {
+        return res.status(500).json({ error: 'Failed to authenticate token.' });
+    }
 });
+
+app.post('/refresh-token', (req, res) => {
+    const { refreshToken } = req.body;
+
+    if (!refreshToken) {
+        return res.status(403).json({ error: 'No refresh token provided.' });
+    }
+    const token = extractToken(refreshToken);
+
+    try {
+        const decoded = jwt.verify(token, JWT_REFRESH_SECRET);
+
+        if (typeof decoded === 'object' && 'id' in decoded) {
+            const accessToken = jwt.sign({ id: decoded.id }, JWT_SECRET, { expiresIn: '15m' });
+
+            return res.json({ accessToken });
+        } else {
+            return res.status(500).json({ error: 'Invalid refresh token.' });
+        }
+    } catch (error) {
+        return res.status(500).json({ error: 'Failed to refresh token.' });
+    }
+});
+
+app.post('/logout', async (req, res) => {
+    const bearerToken = req.headers['authorization'];
+
+    if (!bearerToken) {
+        return res.status(403).json({ error: 'No token provided.' });
+    }
+
+    const token = extractToken(bearerToken);
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        if (typeof decoded === 'object' && 'id' in decoded) {
+            await prisma.user.update({
+                where: { id: decoded.id },
+                data: {
+                    accessToken: null,
+                    refreshToken: null,
+                }
+            });
+
+            return res.json({ message: 'Logged out successfully.' });
+        } else {
+            return res.status(500).json({ error: 'Invalid access token.' });
+        }
+    } catch (error) {
+        return res.status(500).json({ error: 'Failed to authenticate token.' });
+    }
+});
+
+app.post("/test-token", async (req, res) => {
+    const bearerToken = req.headers['authorization'];
+
+    if (!bearerToken) {
+        return res.status(403).json({ error: 'No token provided.' });
+    }
+
+    const token = extractToken(bearerToken);
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        if (typeof decoded === 'object' && 'id' in decoded) {
+            return res.json({ message: 'Access token is valid.' });
+        } else {
+            return res.status(500).json({ error: 'Invalid access token.' });
+        }
+    } catch (error) {
+        return res.status(500).json({ error: 'Failed to authenticate token.' });
+    }
+})
+
 
 app.listen(3000, () => {
     console.log(`Server ready at: http://localhost:3000`);
