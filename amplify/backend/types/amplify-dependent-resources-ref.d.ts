7cfa78fa8983fc2ea1f7a13ea38318073e202182
BarraR3port <barrar3port@gmail.com>
2023-08-11 03:48:12 -0400

    Fix: Actualice el AWS

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "mereketengueapi97724297": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s35582cf5b": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
