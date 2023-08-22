0a5e7d732a3779b35b42284ffe4c6ba56a828d36
BarraR3port <barrar3port@gmail.com>
2023-08-22 19:25:43 -0400

    Fix: Mejora casi final de la lista, quizas se podría optimizar un poco, pero esto ya es bastante funcional

--- src/routes/feedRoute.ts
+++ src/routes/feedRoute.ts
@@ -147,20 +147,11 @@ router.get("/personalized-parties", authenticateTokenMiddleware, async (req: Aut
     const page = Number(req.query.page) || 1;
     const distanceLimit = Number(req.query.distanceLimit) || MAX_DISTANCE;
     const limit = Number(req.query.limit) || 15;
-    const skip = (page - 1) * limit;
 
     const currentUser = await prisma.user.findUnique({
         where: { id: decoded.id },
-        select: {
-            username: true,
-            locationName: true,
-            locationLatitude: true,
-            locationLongitude: true,
-            musicInterest: true,
-            deportsInterest: true,
-            artAndCultureInterest: true,
-            techInterest: true,
-            hobbiesInterest: true
+        include: {
+            followingUserList: true
         }
     });
 
@@ -169,6 +160,8 @@ router.get("/personalized-parties", authenticateTokenMiddleware, async (req: Aut
     }
 
     const currentDateTime = new Date();
+
+    const followedUsers = currentUser.followingUserList.map(follow => follow.followedUserId);
 
     let queryFilters = {
         date: { gte: currentDateTime },
@@ -188,45 +181,56 @@ router.get("/personalized-parties", authenticateTokenMiddleware, async (req: Aut
                     }
                 }
             ]
-        },
-        OR: [
-            { tags: { hasSome: currentUser.musicInterest } },
-            { tags: { hasSome: currentUser.deportsInterest } },
-            { tags: { hasSome: currentUser.artAndCultureInterest } },
-            { tags: { hasSome: currentUser.techInterest } },
-            { tags: { hasSome: currentUser.hobbiesInterest } }
-        ]
+        }
     };
-
     const totalParties = await prisma.party.count({ where: queryFilters });
-    let hasNextPage = ((page - 1) * limit + limit) < totalParties;
 
-    const parties = await prisma.party.findMany({
-        skip: skip,
-        take: limit,
-        orderBy: { date: 'asc' },
-        where: queryFilters
-    });
-
-    const newParties = parties
-        .map(party => {
-            const location = getCoordinatesFromComuna(party.location);
-            if (!location) {
-                return null;
+    const fetchPartiesInRange = async (): Promise<any[]> => {
+        const parties = await prisma.party.findMany({
+            skip: (page - 1) * limit,
+            take: limit,
+            orderBy: { date: 'asc' },
+            where: queryFilters,
+            include: {
+                participants: {
+                    select: {
+                        userId: true
+                    }
+                },
+                owner: {
+                    select: {
+                        username: true
+                    }
+                }
             }
-            const distance = haversineDistance(currentUser.locationLatitude, currentUser.locationLongitude, location.lat, location.lon);
-            const relevanceScore = party.tags.filter(tag => currentUser.musicInterest.includes(tag)).length;
-            return { ...party, distance, relevanceScore };
-        })
-        .filter(party => party !== null && party.distance <= distanceLimit)
-        .sort((a, b) => {
-            const distanceDifference = a!.distance - b!.distance;
-            const relevanceDifference = b!.relevanceScore - a!.relevanceScore;
-            return distanceDifference || relevanceDifference;
         });
 
-    res.status(200).json({ parties: newParties, step: page + 1, reachedMaxItemsInDB: !hasNextPage });
+        return parties
+            .map(party => {
+                const location = getCoordinatesFromComuna(party.location);
+                if (!location || party.ownerId === null) return null;
 
+                const distance = haversineDistance(currentUser.locationLatitude, currentUser.locationLongitude, location.lat, location.lon);
+                let relevanceScore = party.tags.filter(tag => currentUser.musicInterest.includes(tag)).length;
+
+                // Incrementar la relevancia si el dueño o un miembro del party es seguido por el usuario actual
+                if (followedUsers.includes(party.ownerId)) relevanceScore++;
+                if (party.participants.some(participant => followedUsers.includes(participant.userId))) relevanceScore++;
+
+                return { ...party, distance, relevanceScore };
+            })
+            .filter(party => party !== null && party.distance <= distanceLimit)
+            .sort((a, b) => {
+                if (!a || !b) return 0;
+                const distanceDifference = a.distance - b.distance;
+                const relevanceDifference = b.relevanceScore - a.relevanceScore;
+                return relevanceDifference || distanceDifference;
+            });
+    };
+
+    const partiesToReturn = await fetchPartiesInRange();
+
+    res.status(200).json({ parties: partiesToReturn, step: page + 1, reachedMaxItemsInDB: (page * limit >= totalParties) });
 });
 
 
