38421291ac49bd67d684b5588bb79ba26ade8841
BarraR3port <barrar3port@gmail.com>
2023-08-22 18:04:54 -0400

    Feat: Arreglo en la lista de parties

--- src/routes/feedRoute.ts
+++ src/routes/feedRoute.ts
@@ -143,6 +143,7 @@ router.get("/personalized-parties", authenticateTokenMiddleware, async (req: Aut
     if (typeof decoded !== "object" || !("id" in decoded)) {
         return respondWithError(res, 500, "Error al decodificar el token.");
     }
+
     const page = Number(req.query.page) || 1;
     const distanceLimit = Number(req.query.distanceLimit) || MAX_DISTANCE;
     const limit = Number(req.query.limit) || 15;
@@ -168,148 +169,66 @@ router.get("/personalized-parties", authenticateTokenMiddleware, async (req: Aut
     }
 
     const currentDateTime = new Date();
-    const totalParties = await prisma.party.count({
-        where: {
-            date: {
-                gte: currentDateTime // Solo considerar fiestas que no hayan expirado
-            },
-            active: true,
-            OR: [
+
+    let queryFilters = {
+        date: { gte: currentDateTime },
+        active: true,
+        NOT: {
+            private: true,
+            AND: [
+                { creatorUsername: { not: currentUser.username } },
                 {
-                    private: true,
-                    creatorUsername: currentUser.username // El usuario es el creador
-                },
-                {
-                    private: true,
                     moderators: {
-                        some: {
-                            userId: decoded.id // El usuario es un moderador
-                        }
+                        none: { userId: decoded.id }
                     }
                 },
                 {
-                    private: true,
                     participants: {
-                        some: {
-                            userId: decoded.id // El usuario es un participante
-                        }
+                        none: { userId: decoded.id }
                     }
-                },
-                { tags: { hasSome: currentUser.musicInterest } },
-                { tags: { hasSome: currentUser.deportsInterest } },
-                { tags: { hasSome: currentUser.artAndCultureInterest } },
-                { tags: { hasSome: currentUser.techInterest } },
-                { tags: { hasSome: currentUser.hobbiesInterest } },
-            ],
-        },
-        orderBy: { date: 'asc' },
-
-    });
-
-    let filteredParties: any[] = [];
-    let fetchedPartyIds = new Set<string>();
-    let step = page;  // To keep track of the number of attempts
-    let reachedMaxItemsInDB = false;
-    const maxSteps = Math.ceil(totalParties / limit) + 1;
-    const timeoutReached = new Promise((_, reject) => setTimeout(() => {
-        reachedMaxItemsInDB = true;
-        return reject(new Error("Timeout exceeded"));
-    }, 5000));
-    const fetchParties = async () => {
-        while (filteredParties.length < limit && step <= maxSteps) {
-            const parties = await prisma.party.findMany({
-                skip: (step - 1) * limit,  // Correct calculation for skip
-                take: limit,
-                orderBy: { date: 'asc' },
-                where: {
-                    active: true,
-                    date: {
-                        gte: currentDateTime // Solo considerar fiestas que no hayan expirado
-                    },
-                    NOT: {
-                        id: {
-                            in: Array.from(fetchedPartyIds)  // Excluir parties ya recuperadas
-                        }
-                    },
-                    OR: [
-                        {
-                            private: true,
-                            creatorUsername: currentUser.username // El usuario es el creador
-                        },
-                        {
-                            private: true,
-                            moderators: {
-                                some: {
-                                    userId: decoded.id // El usuario es un moderador
-                                }
-                            }
-                        },
-                        {
-                            private: true,
-                            participants: {
-                                some: {
-                                    userId: decoded.id // El usuario es un participante
-                                }
-                            }
-                        },
-                        { tags: { hasSome: currentUser.musicInterest } },
-                        { tags: { hasSome: currentUser.deportsInterest } },
-                        { tags: { hasSome: currentUser.artAndCultureInterest } },
-                        { tags: { hasSome: currentUser.techInterest } },
-                        { tags: { hasSome: currentUser.hobbiesInterest } },
-                    ],
                 }
-            });
-            //console.log("\nFound parties:", parties.length, "Step:", step, "Skip:", (step - 1) * limit, "Limit:", limit, "Distance limit:", distanceLimit, "km");
-            const currentFilteredParties = parties
-                .map(party => {
-                    const location = getCoordinatesFromComuna(party.location);
-                    if (!location) {
-                        return null;
-                    }
-                    const distance = haversineDistance(currentUser.locationLatitude, currentUser.locationLongitude, location.lat, location.lon);
-                    return { ...party, distance };
-                })
-                .filter(party => party !== null && party.distance <= distanceLimit)
-                .sort((a, b) => a!.distance - b!.distance);
-            currentFilteredParties.forEach(party => {
-                if (party !== null) fetchedPartyIds.add(party.id);
-            });
-
-
-            filteredParties = [...filteredParties, ...currentFilteredParties];
-
-            if (parties.length < limit) {
-                reachedMaxItemsInDB = true;
-                console.log("Reached max items in DB", parties.length, limit, filteredParties.length);
-                console.log();
-                break;
-            }
-
-            step++;
-        }
+            ]
+        },
+        OR: [
+            { tags: { hasSome: currentUser.musicInterest } },
+            { tags: { hasSome: currentUser.deportsInterest } },
+            { tags: { hasSome: currentUser.artAndCultureInterest } },
+            { tags: { hasSome: currentUser.techInterest } },
+            { tags: { hasSome: currentUser.hobbiesInterest } }
+        ]
     };
 
-    try {
-        await Promise.race([fetchParties(), timeoutReached]);
-    } catch (error) {
-        if (error instanceof Error && error.message === "Timeout exceeded") {
-            console.warn("La consulta excedió el tiempo máximo de espera. Retornando lo que tenemos.");
-            // Aquí puedes decidir qué hacer, por ejemplo, retornar lo que tienes:
-            res.status(200).json({ parties: filteredParties, step });
-            return;
-        }
-        // Manejar otros errores
-        console.error(error);
-        return respondWithError(res, 500, "Error fetching parties.");
-    }
-    // Take only the required limit of parties
-    filteredParties = filteredParties.slice(0, limit);
-    //console.log("Found parties:", filteredParties.length, "Requested Page", page, "Step:", `${step}/${maxSteps}`, "Skip:", (step - 1) * limit, "Limit:", limit, "Distance limit:", distanceLimit, "km");
+    const totalParties = await prisma.party.count({ where: queryFilters });
+    let hasNextPage = ((page - 1) * limit + limit) < totalParties;
 
-    res.status(200).json({ parties: filteredParties, step, reachedMaxItemsInDB });
+    const parties = await prisma.party.findMany({
+        skip: skip,
+        take: limit,
+        orderBy: { date: 'asc' },
+        where: queryFilters
+    });
+
+    const newParties = parties
+        .map(party => {
+            const location = getCoordinatesFromComuna(party.location);
+            if (!location) {
+                return null;
+            }
+            const distance = haversineDistance(currentUser.locationLatitude, currentUser.locationLongitude, location.lat, location.lon);
+            const relevanceScore = party.tags.filter(tag => currentUser.musicInterest.includes(tag)).length;
+            return { ...party, distance, relevanceScore };
+        })
+        .filter(party => party !== null && party.distance <= distanceLimit)
+        .sort((a, b) => {
+            const distanceDifference = a!.distance - b!.distance;
+            const relevanceDifference = b!.relevanceScore - a!.relevanceScore;
+            return distanceDifference || relevanceDifference;
+        });
+
+    res.status(200).json({ parties: newParties, step: page + 1, reachedMaxItemsInDB: !hasNextPage });
 
 });
+
 
 router.get("/personalized-parties2", authenticateTokenMiddleware, async (req: AuthenticatedRequest, res: Response) => {
     const decoded = req.decoded;
