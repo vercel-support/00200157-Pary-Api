d62d8250c90d85528ed42da734e82e041f4895fa
BarraR3port <barrar3port@gmail.com>
2023-09-12 20:12:01 -0300

    Feat: Migracions del endpoint Feedh

--- app/src/user/user.module.ts
+++ app/src/user/user.module.ts
@@ -2,9 +2,9 @@ import {MiddlewareConsumer, Module, NestModule} from "@nestjs/common";
 import {UserController} from "./controllers/user.controller";
 import {AuthTokenMiddleware} from "app/middlewares/auth-token/auth-token.middleware";
 import {UserService} from "app/services/user/user.service";
-import {PrismaModule} from "../prisma/prisma.module";
-import {UtilsModule} from "../utils/utils.module";
-import {NotificationsModule} from "../notifications/notifications.module";
+import {PrismaModule} from "../../controllers/prisma/prisma.module";
+import {UtilsModule} from "../../controllers/utils/utils.module";
+import {NotificationsModule} from "../../controllers/notifications/notifications.module";
 
 @Module({
     controllers: [UserController],
