8cc86d923ad5bc25a5a991186ca2a1e813542655
BarraR3port <barrar3port@gmail.com>
2023-09-13 08:21:33 -0300

    Feat: Migracions del endpoint Party y Group

--- app/src/group/group.module.ts
+++ app/src/group/group.module.ts
@@ -1,9 +1,11 @@
 import {MiddlewareConsumer, Module, NestModule} from "@nestjs/common";
 import {AuthTokenMiddleware} from "app/middlewares/auth-token/auth-token.middleware";
 import {GroupController} from "./controllers/group.controller";
+import {GroupService} from "app/services/group/group.service";
 
 @Module({
     controllers: [GroupController],
+    providers: [GroupService],
 })
 export class GroupModule implements NestModule {
     configure(consumer: MiddlewareConsumer) {
--- app/src/party/party.module.ts
+++ app/src/party/party.module.ts
@@ -1,9 +1,11 @@
 import {MiddlewareConsumer, Module, NestModule} from "@nestjs/common";
 import {PartyController} from "./controllers/party.controller";
 import {AuthTokenMiddleware} from "app/middlewares/auth-token/auth-token.middleware";
+import {PartyService} from "app/services/party/party.service";
 
 @Module({
     controllers: [PartyController],
+    providers: [PartyService],
 })
 export class PartyModule implements NestModule {
     configure(consumer: MiddlewareConsumer) {
