2a51d72fba4475c2661e7a6c83d5d1361ad85a81
BarraR3port <barrar3port@gmail.com>
2023-09-13 12:15:02 -0300

    Feat: Avance en la migracion de party

--- app/src/party/controllers/party.controller.ts
+++ app/src/party/controllers/party.controller.ts
@@ -1,4 +1,50 @@
-import {Controller} from "@nestjs/common";
+import {Body, Controller, Get, Param, Post, Query, Req, UsePipes, ValidationPipe} from "@nestjs/common";
+import {CreatePartyDto} from "app/dtos/party/CreateParty.dto";
+import {PartyService} from "app/services/party/party.service";
 
 @Controller("party")
-export class PartyController {}
+export class PartyController {
+    constructor(private readonly partyService: PartyService) {}
+
+    @Get("own-parties")
+    async getOwnParties(@Query("page") page: number, @Query("limit") limit: number, @Req() request: any) {
+        return this.partyService.getOwnParties(page, limit, request.raw.decoded.id);
+    }
+
+    @Post("create")
+    @UsePipes(new ValidationPipe())
+    async createParty(@Body("party") party: CreatePartyDto, @Req() request: any) {
+        return this.partyService.createParty(party, request.raw.decoded.id);
+    }
+
+    @Post("upload-party-image")
+    @UsePipes(new ValidationPipe())
+    async uploadPartyImage(@Body() body: any) {
+        return this.partyService.uploadPartyImage(body);
+    }
+
+    @Get("invited-parties")
+    async getInvitedParties(@Query("page") page: number, @Query("limit") limit: number, @Req() request: any) {
+        return this.partyService.getInvitedParties(page, limit, request.raw.decoded.id);
+    }
+
+    @Get(":partyId")
+    async getParty(@Param("partyId") partyId: string, @Req() request: any) {
+        return this.partyService.getParty(partyId, request.raw.decoded.id);
+    }
+
+    @Post(":partyId/leave")
+    async leaveParty(@Param("partyId") partyId: string, @Req() request: any) {
+        return this.partyService.leaveParty(partyId, request.raw.decoded.id);
+    }
+
+    @Post(":partyId/accept-invitation")
+    async acceptInvitation(@Param("partyId") partyId: string, @Req() request: any) {
+        return this.partyService.acceptInvitation(partyId, request.raw.decoded.id);
+    }
+
+    @Post(":partyId/decline-invitation")
+    async declineInvitation(@Param("partyId") partyId: string, @Req() request: any) {
+        return this.partyService.declineInvitation(partyId, request.raw.decoded.id);
+    }
+}
