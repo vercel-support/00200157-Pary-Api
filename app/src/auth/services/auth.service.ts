c21be6993fd86926b64c169531ba9341703646fd
Benjamin <BPerez51511a@gmail.com>
2024-01-20 19:03:40 -0300

    refactor: change Auth_wen_token to Auth_web_token

--- app/src/auth/services/auth.service.ts
+++ app/src/auth/services/auth.service.ts
@@ -10,120 +10,122 @@ const client = new OAuth2Client();
 
 @Injectable()
 export class AuthService {
-	constructor(
-		private prisma: PrismaService,
-		private utils: UtilsService
-	) {}
+  constructor(private prisma: PrismaService, private utils: UtilsService) {}
 
-	async signInUser(googleUser: GoogleUser) {
-		const { idToken } = googleUser;
+  async signInUser(googleUser: GoogleUser) {
+    const { idToken } = googleUser;
 
-		const ticket = await client.verifyIdToken({
-			idToken,
-			audience: process.env.GOOGLE_AUTH_WEN_TOKEN
-		});
-		const payload = ticket.getPayload();
-		if (!payload) throw new InternalServerErrorException("Invalid access token.");
+    const ticket = await client.verifyIdToken({
+      idToken,
+      audience: process.env.GOOGLE_AUTH_WEB_TOKEN,
+    });
+    const payload = ticket.getPayload();
+    if (!payload)
+      throw new InternalServerErrorException("Invalid access token.");
 
-		let user: any = await this.prisma.user.findUnique({
-			where: {
-				assignedGoogleID: googleUser.user.id
-			},
-			select: {
-				id: true
-			}
-		});
+    let user: any = await this.prisma.user.findUnique({
+      where: {
+        assignedGoogleID: googleUser.user.id,
+      },
+      select: {
+        id: true,
+      },
+    });
 
-		if (!user) {
-			const generatedUsername = `${googleUser.user.givenName ?? ""}${
-				googleUser.user.familyName ?? ""
-			}${Math.floor(Math.random() * (9999 - 1000 + 1) + 1000)}`;
+    if (!user) {
+      const generatedUsername = `${googleUser.user.givenName ?? ""}${
+        googleUser.user.familyName ?? ""
+      }${Math.floor(Math.random() * (9999 - 1000 + 1) + 1000)}`;
 
-			const userLocation = await this.prisma.location.create({
-				data: {
-					name: "",
-					latitude: 0,
-					longitude: 0,
-					timestamp: new Date(),
-					address: ""
-				}
-			});
+      const userLocation = await this.prisma.location.create({
+        data: {
+          name: "",
+          latitude: 0,
+          longitude: 0,
+          timestamp: new Date(),
+          address: "",
+        },
+      });
 
-			user = await this.prisma.$transaction([
-				this.prisma.user.create({
-					data: {
-						username: generatedUsername,
-						name: googleUser.user.givenName ?? "",
-						email: googleUser.user.email,
-						lastName: googleUser.user.familyName ?? "",
-						assignedGoogleID: googleUser.user.id,
-						lastLogin: new Date(),
-						createdAt: new Date(),
-						birthDate: new Date(),
-						socialMedia: {
-							instagram: ""
-						},
-						locationId: userLocation.id
-					},
-					include: this.utils.getUserFields()
-				})
-			]);
-		}
+      user = await this.prisma.$transaction([
+        this.prisma.user.create({
+          data: {
+            username: generatedUsername,
+            name: googleUser.user.givenName ?? "",
+            email: googleUser.user.email,
+            lastName: googleUser.user.familyName ?? "",
+            assignedGoogleID: googleUser.user.id,
+            lastLogin: new Date(),
+            createdAt: new Date(),
+            birthDate: new Date(),
+            socialMedia: {
+              instagram: "",
+            },
+            locationId: userLocation.id,
+          },
+          include: this.utils.getUserFields(),
+        }),
+      ]);
+    }
 
-		const accessToken = sign({ id: user.id }, JWT_SECRET, { expiresIn: "1d" });
-		const refreshToken = sign({ id: user.id }, JWT_REFRESH_SECRET, { expiresIn: "4weeks" });
+    const accessToken = sign({ id: user.id }, JWT_SECRET, { expiresIn: "1d" });
+    const refreshToken = sign({ id: user.id }, JWT_REFRESH_SECRET, {
+      expiresIn: "4weeks",
+    });
 
-		user = await this.prisma.user.update({
-			where: { id: user.id },
-			data: {
-				accessToken,
-				refreshToken,
-				lastLogin: new Date()
-			},
-			include: this.utils.getUserFields()
-		});
+    user = await this.prisma.user.update({
+      where: { id: user.id },
+      data: {
+        accessToken,
+        refreshToken,
+        lastLogin: new Date(),
+      },
+      include: this.utils.getUserFields(),
+    });
 
-		console.log("User: ", user)
+    console.log("User: ", user);
 
-		return user;
-	}
+    return user;
+  }
 
-	async logoutUser(userId: string) {
-		const response = await this.prisma.user.update({
-			where: { id: userId },
-			data: {
-				accessToken: "",
-				refreshToken: "",
-				expoPushToken: "",
-				webSocketId: ""
-			}
-		});
+  async logoutUser(userId: string) {
+    const response = await this.prisma.user.update({
+      where: { id: userId },
+      data: {
+        accessToken: "",
+        refreshToken: "",
+        expoPushToken: "",
+        webSocketId: "",
+      },
+    });
 
-		const currentToken = await this.prisma.user.findUnique({
-			where: { id: userId },
-			select: {
-				accessToken: true
-			}
-		});
-		return true;
-	}
+    const currentToken = await this.prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        accessToken: true,
+      },
+    });
+    return true;
+  }
 
-	async refreshToken(userId: string) {
-		const accessToken = sign({ id: userId }, JWT_SECRET, { expiresIn: "1d" });
-		const refreshToken = sign({ id: userId }, JWT_REFRESH_SECRET, { expiresIn: "4weeks" });
-		const user = await this.prisma.user.update({
-			where: { id: userId },
-			data: {
-				accessToken,
-				refreshToken
-			},
-			include: this.utils.getUserFields()
-		});
+  async refreshToken(userId: string) {
+    const accessToken = sign({ id: userId }, JWT_SECRET, { expiresIn: "1d" });
+    const refreshToken = sign({ id: userId }, JWT_REFRESH_SECRET, {
+      expiresIn: "4weeks",
+    });
+    const user = await this.prisma.user.update({
+      where: { id: userId },
+      data: {
+        accessToken,
+        refreshToken,
+      },
+      include: this.utils.getUserFields(),
+    });
 
-		return user;
-	}
+    return user;
+  }
 
-	async createToken(userId: string) {
-		return sign({ id: userId }, JWT_SECRET, { expiresIn: "1d" });
-	}
+  async createToken(userId: string) {
+    return sign({ id: userId }, JWT_SECRET, { expiresIn: "1d" });
+  }
 }
